49657f3a264f52dd60e9b3e116d5841f9f3f09e3
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-10-04 10:43:27 +0600

    practice

--- countries/src/components/Countries/Countries.js
+++ countries/src/components/Countries/Countries.js
@@ -1,34 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
 
-    const [countries, setCountries] = useState([])
+    useEffect( ()=>{
+        
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data));
 
-  useEffect( () => {
-      fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCountries(data))
-  }, [])
+    }, [])
 
     return (
         <div>
-            <h1>Countries Using REST API</h1>
-            <h3>Number of countries: {countries.length}</h3>
-            
+            <h2>Hello Mello Yello Gello from Countries: {countries.length}</h2>
+            <div className='countries-container'>
             {
-              countries.map(country => console.log(country))
+                countries.map(country => <Country 
+                    country={country}
+                    key={country.cca3}
+                    ></Country>)
             }
-            
-      {
-          countries.map(
-            country => <Country 
-
-            country = {country} 
-            key={country.cca3}
-            
-            ></Country>)
-      }
+            </div>
         </div>
     );
 };
--- countries/src/components/Country/Country.js
+++ countries/src/components/Country/Country.js
@@ -2,28 +2,16 @@ import React from 'react';
 import './Country.css';
 
 const Country = (props) => {
-    // console.log(props.country);
-    // const {area} = props.country;
+   const {area, region, name, flags} = props.country;
+
     return (
-        <div className='country'>
-            {/* <p>Area {area}</p> */}
-            {/* <p>Area {name.common}</p> */}
-            <p>a</p>
+        <div className='country bg-warning'>
+            <h2>Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Area: {area}</p>
+            <p><small>Region: {region}</small></p>
         </div>
     );
 };
 
 export default Country;
-
-/* 
-
-<h1>Country Name: {props.name}</h1> 
-      <h3>Capital: {props.capital}</h3> 
-      <h4>Region: {props.region}</h4> 
-      <h4>Population: {props.population} Person</h4> 
-      <h4>Subregion: {props.subregion} </h4> 
-      <img src={props.flags} alt="asdds" /> <br />
-      <p>Currency: {props.currencies} </p> 
-      <small>Area: {props.area}</small> 
-
-*/
