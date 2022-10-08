5e88b2cec6144e5aa216056c521854facc44874f
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-10-08 21:47:37 +0600

    first commit

--- countries/src/components/Countries/Countries.js
+++ countries/src/components/Countries/Countries.js
@@ -15,7 +15,7 @@ const Countries = () => {
 
     return (
         <div>
-            <h2>Hello from Countries: {countries.length}</h2>
+            <h2>Hello from Countriessss: {countries.length}</h2>
             <div className='countries-container'>
             {
                 countries.map(country => <Country 
