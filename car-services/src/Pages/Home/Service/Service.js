bea1631febcf104df438521cfe64a8bb5edfe039
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-10-25 16:28:36 +0600

    car mechanics update

---
+++ car-services/src/Pages/Home/Service/Service.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Service.css';
+const Service = ({service}) => {
+    const {name, img, description, price} = service;
+    return (
+        <div className='service'>
+            <img src={img} alt="" />
+            <h2>{name}</h2>
+            <h4>${price}</h4>
+            <p>{description}</p>
+            <button>Book this service</button>
+        </div>
+    );
+};
+
+export default Service;
