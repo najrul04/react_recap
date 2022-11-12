d75720508ebc832bc8d1375bc06c0e0f8a331775
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-11-12 21:22:50 +0600

    company done

--- car-services/src/Pages/Home/Company/Company.js
+++ car-services/src/Pages/Home/Company/Company.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './Expert.css';
-const Expert = ({expert}) => {
-    const {name, img, company} = expert
+import './Company.css';
+const Company = ({company}) => {
+    const {name, img, companies} = company;
     return (
         <div className='g-5 col-sm-12 col-md-6 col-lg-4'>
             <div className="card" style={{ width: "18rem" }}>
                 <img src={img} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title text-center">{name}</h5>
-                    <p className="card-text">{company}</p>
+                    <p className="card-text">{companies}</p>
                     <a href="#" className="btn btn-dark">See More</a>
                 </div>
             </div>
         </div>
     );
 };
-export default Expert;
+export default Company;
--- car-services/src/Pages/Home/Experts/Experts.js
+++ car-services/src/Pages/Home/Experts/Experts.js
@@ -5,16 +5,16 @@ import company3 from '../../../images/experts/company3.jpg';
 import company4 from '../../../images/experts/company4.jpg';
 import company5 from '../../../images/experts/company5.jpg';
 import company6 from '../../../images/experts/company6.jpg';
-import Expert from '../Expert/Expert';
+import Company from '../Company/Company';
 import './Experts.css';
 
 const experts = [
-    {id: 1, name: 'Ubisoft', img: company1, company: 'Ubisoft has become one of the largest video game publishers, and it has the largest in-house development team'},
-    {id: 2, name: 'Nintendo Switch', img: company2, company: 'Nintendos mission is to put smiles on the faces of everyone we touch.The company is known for worldwide.'},
-    {id: 3, name: 'Sony', img: company3, company: 'Sony Interactive Entertainment handles the research and development.Spider Man is the most recent one.'},
-    {id: 4, name: 'Tencent', img: company4, company: 'Tencent is a world-leading internet and technology companyto produce more user friendly game.'},
-    {id: 5, name: 'EA Sports', img: company5, company: 'EA Sports is a division of Electronic Arts that develops and publishes sports video games.'},
-    {id: 6, name: 'Activision', img: company6, company: 'Activision Publishing, Inc. is an American video game publisher based in Santa Monica, California.'},
+    {id: 1, name: 'Ubisoft', img: company1, companies: 'Ubisoft has become one of the largest video game publishers, and it has the largest in-house development team'},
+    {id: 2, name: 'Nintendo Switch', img: company2, companies: 'Nintendos mission is to put smiles on the faces of everyone we touch.The company is known for worldwide.'},
+    {id: 3, name: 'Sony', img: company3, companies: 'Sony Interactive Entertainment handles the research and development.Spider Man is the most recent one.'},
+    {id: 4, name: 'Tencent', img: company4, companies: 'Tencent is a world-leading internet and technology companyto produce more user friendly game.'},
+    {id: 5, name: 'EA Sports', img: company5, companies: 'EA Sports is a division of Electronic Arts that develops and publishes sports video games.'},
+    {id: 6, name: 'Activision', img: company6, companies: 'Activision Publishing, Inc. is an American video game publisher based in Santa Monica, California.'},
 ]
 
 const Experts = () => {
@@ -23,9 +23,9 @@ const Experts = () => {
             <h2 className='text-dark text-center mt-5'>Game Companies</h2>
             <div className="row">
                 {
-                    experts.map(expert=> <Expert
-                    key={expert.id}
-                    expert={expert}
+                    experts.map(company=> <Company
+                    key={company.id}
+                    company={company}
                     />)
                 }
         </div>
