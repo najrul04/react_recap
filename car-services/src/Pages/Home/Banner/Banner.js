0ce65750e54adabf3d582f4e1a2ad7217bdc2e5f
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-12-24 14:41:39 +0600

    add new banners

--- car-services/src/Pages/Home/Banner/Banner.js
+++ car-services/src/Pages/Home/Banner/Banner.js
@@ -4,6 +4,9 @@ import { Carousel } from 'react-bootstrap';
 import banner1 from '../../../images/banner/banner1.jpg';
 import banner2 from '../../../images/banner/banner2.jpg';
 import banner3 from '../../../images/banner/banner3.jpg';
+import banner4 from '../../../images/banner/banner4.jpg';
+import banner5 from '../../../images/banner/banner5.jpg';
+import banner6 from '../../../images/banner/banner6.jpg';
 
 const Banner = () => {
     const [index, setIndex] = useState(0);
@@ -43,6 +46,40 @@ const Banner = () => {
 
                 <Carousel.Caption>
                     <h3>Batman</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner4}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>FIFA 18</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner5}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Red Dead Redemption</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner6}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Elden Rings</h3>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
